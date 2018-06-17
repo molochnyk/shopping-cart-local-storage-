e070efaaddb5bef77bb1507b53a5ff91dbb8cd97
Molochyk <slapibs18@gmail.com>
2018-06-17 13:06:33 +0300

    data to local-storage

--- app/js/common.min.js
+++ app/js/common.min.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-
 	
 }
+
 //-----VARIBLE
 const shoppingBox = document.querySelector('.js-shoppingBox');
 const shoppingBtn = document.querySelector('.js-shoppingBtn');
@@ -11,7 +11,6 @@ const shoppingWrap = document.querySelector('.shopping_wrap');
 const goodsClear = document.querySelector('.js-goodsClear');
 
 
-
 //-----LISTENERS
 addEvent()
 function addEvent() {
@@ -19,9 +18,9 @@ function addEvent() {
 	getShopBtnClose.addEventListener('click', shoppingClose);
 	goodsList.addEventListener('click',goodCart );
 	shoppingWrap.addEventListener('click', removeProd);
-	goodsClear.addEventListener('click', goodsAllClear)
+	goodsClear.addEventListener('click', goodsAllClear);
+	document.addEventListener("DOMContentLoaded", getReadyLocalStorage);
 };
-
 
 
 
@@ -64,18 +63,77 @@ function addToCart(data) {
 		<button class="js-removeProd" data-id="${data.id}">&times;</button>
 	`;
 	shoppingWrap.appendChild(item);
+	//Добавляем товар в локалСтораже
+	saveLocalStorage(data)
 };
+//Запись ЛОКАЛ СТОРАЖ
+function saveLocalStorage(data) {
+	let prod = getLocalStorage();
+	prod.push(data);
+  localStorage.setItem('goods', JSON.stringify(prod))
+}
+//Проверка ЛОКАЛ СТОРАЖ
+function getLocalStorage() {
+	let goods;
+  if(localStorage.getItem('goods') === null) {
+    goods = [];
+  } else {
+    goods = JSON.parse(localStorage.getItem('goods'))
+  }
+  return goods;
+}
+
 
 //Удаляем товар из картизны по крестику
 function removeProd(e) {
+	let prod;
+	let prodsID;
 	if(e.target.classList.contains('js-removeProd')) {
-		e.target.parentElement.remove()
+		e.target.parentElement.remove();
+		prod = e.target.parentElement;
+		prodsID = prod.querySelector('button').getAttribute('data-id')
 	}
+	removeLocalStorage(prodsID)
 }
+//Удаляем товар из картизны по крестику(LOCAL STORAGE)
+function removeLocalStorage(id) {
+	let goods = getLocalStorage();
+	goods.forEach((el, i) => {
+    if(el.id === id) {
+      goods.splice(i, 1);
+    }
+  });
+  localStorage.setItem('goods', JSON.stringify(goods))
+}
+
 
 //Удаляем товарЫ из картизны по кнопке
 function goodsAllClear() {
 	while(shoppingWrap.firstElementChild) {
 		shoppingWrap.removeChild(shoppingWrap.firstElementChild)
 	}
+	clearLocalStorege();
+}
+//Удаляем товарЫ из картизны по кнопке(LOCAL STORAGE)
+function clearLocalStorege() {
+	localStorage.clear()
+}
+
+
+//ОТРИСОВУЕМ данные из ЛОКАЛ СТОДАЖА
+function getReadyLocalStorage() {
+	let goods = getLocalStorage();
+	goods.forEach(data => {
+		const item = document.createElement('div');
+		item.className = 'shopping_item';
+		item.innerHTML = `
+			<img src="${data.img}" width="100" alt="${data.title}">
+			<div class="shopping_item_cont">
+				<h5>${data.title}</h5>
+				<p>${data.price}грн</p>
+			</div>
+			<button class="js-removeProd" data-id="${data.id}">&times;</button>
+		`;
+		shoppingWrap.appendChild(item);
+	});
 }
